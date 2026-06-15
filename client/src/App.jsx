737a5e405520112d6b1c7ac48e9cf463f3ad4352
SoraPewnaldo <SoraPewnaldo@users.noreply.github.com>
2026-06-15 16:47:57 +0530

    build: switch to HashRouter for GitHub Pages compat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Spline from '@splinetool/react-spline';
 import { SettingsProvider } from './context/SettingsContext';
 import Layout from './components/Layout';
